ae17fd8f9646e799da091406a2db351643af119d
Shaquil Faizal <shaquil.faizal@gmail.com>
2020-06-29 15:32:35 +0530

    update CategoryRepo

--- src/main/java/com/shaquilfaizal/RecipesApp/repositories/CategoryRepository.java
+++ src/main/java/com/shaquilfaizal/RecipesApp/repositories/CategoryRepository.java
@@ -1,9 +1,13 @@
 package com.shaquilfaizal.RecipesApp.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.shaquilfaizal.RecipesApp.domain.Category;
 
+
 public interface CategoryRepository extends CrudRepository<Category, Long> {
 
+	Optional<Category> findByDescription(String description);
 }
