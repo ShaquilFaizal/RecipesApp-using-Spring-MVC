557f98537da44b05058b103cddff07fad44b85f1
Shaquil Faizal <shaquil.faizal@gmail.com>
2020-07-02 07:59:54 +0530

    update Category model

--- src/main/java/com/shaquilfaizal/RecipesApp/domain/Category.java
+++ src/main/java/com/shaquilfaizal/RecipesApp/domain/Category.java
@@ -11,42 +11,22 @@ import javax.persistence.ManyToMany;
 import lombok.Getter;
 import lombok.Setter;
 
-
 @Entity
 public class Category {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Getter @Setter private Long id;
-	
-	@Getter @Setter private String description;
-	
+	@Getter
+	@Setter
+	private Long id;
+
+	@Getter
+	@Setter
+	private String description;
+
 	@ManyToMany(mappedBy = "categories")
-	@Getter @Setter private Set<Recipe> recipes;
+	@Getter
+	@Setter
+	private Set<Recipe> recipes;
 
-/*	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-
-	public Set<Recipe> getRecipes() {
-		return recipes;
-	}
-
-	public void setRecipes(Set<Recipe> recipes) {
-		this.recipes = recipes;
-	}
-	*/
-	
 }
