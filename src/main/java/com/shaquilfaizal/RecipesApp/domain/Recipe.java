fa861b1ae25cbdef432b45cb5ab97b89299c2ec8
Shaquil Faizal <shaquil.faizal@gmail.com>
2020-07-02 07:48:53 +0530

    organize recipe model

--- src/main/java/com/shaquilfaizal/RecipesApp/domain/Recipe.java
+++ src/main/java/com/shaquilfaizal/RecipesApp/domain/Recipe.java
@@ -25,40 +25,58 @@ public class Recipe {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Getter @Setter private Long id;
-	
-	@Getter @Setter private String description;
-	@Getter @Setter private Integer prepTime;
-	@Getter @Setter private Integer cookTime;
-	@Getter @Setter private Integer servings;
-	@Getter @Setter private String source;
-	@Getter @Setter private String url;
-	
+	@Getter
+	@Setter
+	private Long id;
+
+	@Getter
+	@Setter
+	private String description;
+	@Getter
+	@Setter
+	private Integer prepTime;
+	@Getter
+	@Setter
+	private Integer cookTime;
+	@Getter
+	@Setter
+	private Integer servings;
+	@Getter
+	@Setter
+	private String source;
+	@Getter
+	@Setter
+	private String url;
+
 	@Lob
-	@Getter @Setter private String directions;
-	
-	@OneToMany(cascade = CascadeType.ALL,mappedBy = "recipe")
-	@Getter @Setter private Set<Ingredient> ingredients = new HashSet<>();
-	
+	@Getter
+	@Setter
+	private String directions;
+
+	@OneToMany(cascade = CascadeType.ALL, mappedBy = "recipe")
+	@Getter
+	@Setter
+	private Set<Ingredient> ingredients = new HashSet<>();
+
 	@Lob
-	@Getter @Setter private Byte[] image;
-	
+	@Getter
+	@Setter
+	private Byte[] image;
+
 	@OneToOne(cascade = CascadeType.ALL)
-	@Getter @Setter private Notes notes;
-	
+	@Getter
+	@Setter
+	private Notes notes;
+
 	@Enumerated(value = EnumType.STRING)
-	@Getter @Setter private Difficulty difficulty;
-	
+	@Getter
+	@Setter
+	private Difficulty difficulty;
+
 	@ManyToMany
-	@JoinTable(name = "recipe_category",
-		joinColumns = @JoinColumn(name="recipe_id"), inverseJoinColumns = @JoinColumn(name="category_id"))
-	@Getter @Setter private Set<Category> categories = new HashSet<>();
+	@JoinTable(name = "recipe_category", joinColumns = @JoinColumn(name = "recipe_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
+	@Getter
+	@Setter
+	private Set<Category> categories = new HashSet<>();
 
-
-	
-
-
-	
-	
-	
 }
