2c120713853196e289abebbb68534be6843ed17d
Shaquil Faizal <shaquil.faizal@gmail.com>
2020-07-02 08:02:06 +0530

    Update RecipeService interface

--- src/main/java/com/shaquilfaizal/RecipesApp/services/RecipeService.java
+++ src/main/java/com/shaquilfaizal/RecipesApp/services/RecipeService.java
@@ -2,7 +2,10 @@ package com.shaquilfaizal.RecipesApp.services;
 
 import java.util.Set;
 
+import org.springframework.stereotype.Service;
+
 import com.shaquilfaizal.RecipesApp.domain.Recipe;
+
 
 public interface RecipeService {
 
