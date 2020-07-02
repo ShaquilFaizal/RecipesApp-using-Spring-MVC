33dda96c9a9f235614bc3db6ced765551dcb74f8
Shaquil Faizal <shaquil.faizal@gmail.com>
2020-07-02 07:59:03 +0530

    refactor unitOfMeausure model with lombok

--- src/main/java/com/shaquilfaizal/RecipesApp/domain/UnitOfMeasure.java
+++ src/main/java/com/shaquilfaizal/RecipesApp/domain/UnitOfMeasure.java
@@ -5,29 +5,20 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+import lombok.Getter;
+import lombok.Setter;
+
 @Entity
 public class UnitOfMeasure {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Getter
+	@Setter
 	private Long id;
-	
+
+	@Getter
+	@Setter
 	private String description;
 
-	public Long getId() {
-		return id;
-	}
-
-	public void setId(Long id) {
-		this.id = id;
-	}
-
-	public String getDescription() {
-		return description;
-	}
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
 }
-	
