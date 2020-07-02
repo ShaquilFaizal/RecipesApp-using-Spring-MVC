6a8d29f3b17c5883a35829a8bc84dfff98386ec6
Shaquil Faizal <shaquil.faizal@gmail.com>
2020-07-02 07:08:54 +0530

    update lombok refactoring

--- src/main/java/com/shaquilfaizal/RecipesApp/domain/Category.java
+++ src/main/java/com/shaquilfaizal/RecipesApp/domain/Category.java
@@ -8,20 +8,45 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+
 @Entity
 public class Category {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+	@Getter @Setter private Long id;
 	
-	private String description;
+	@Getter @Setter private String description;
 	
 	@ManyToMany(mappedBy = "categories")
-	private Set<Recipe> recipes;
-	
+	@Getter @Setter private Set<Recipe> recipes;
+
+/*	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public Set<Recipe> getRecipes() {
+		return recipes;
+	}
+
+	public void setRecipes(Set<Recipe> recipes) {
+		this.recipes = recipes;
+	}
+	*/
 	
 }
