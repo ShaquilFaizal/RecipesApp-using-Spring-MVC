26021deff537c462174a53e4c2f4f288a3dd8937
Shaquil Faizal <shaquil.faizal@gmail.com>
2020-08-31 08:59:56 +0530

    added commands

---
+++ src/main/java/com/shaquilfaizal/RecipesApp/commands/NotesCommand.java
@@ -0,0 +1,14 @@
+package com.shaquilfaizal.RecipesApp.commands;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class NotesCommand {
+    private Long id;
+    private String recipeNotes;
+
+}
