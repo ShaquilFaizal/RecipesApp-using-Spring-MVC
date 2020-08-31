2238d515fd2d4c7ea5cb70cad0671e2449886780
Shaquil Faizal <shaquil.faizal@gmail.com>
2020-08-31 14:38:50 +0530

    added saveCommand implementation

--- src/main/java/com/shaquilfaizal/RecipesApp/services/RecipeServiceImpl.java
+++ src/main/java/com/shaquilfaizal/RecipesApp/services/RecipeServiceImpl.java
@@ -5,7 +5,11 @@ import java.util.Optional;
 import java.util.Set;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.shaquilfaizal.RecipesApp.commands.RecipeCommand;
+import com.shaquilfaizal.RecipesApp.converters.RecipeCommandToRecipe;
+import com.shaquilfaizal.RecipesApp.converters.RecipeToRecipeCommand;
 import com.shaquilfaizal.RecipesApp.domain.Recipe;
 import com.shaquilfaizal.RecipesApp.repositories.RecipeRepository;
 
@@ -16,13 +20,14 @@ import lombok.extern.slf4j.Slf4j;
 public class RecipeServiceImpl implements RecipeService{
 
 	private final RecipeRepository recipeRepository;
-	
-	
-	public RecipeServiceImpl(RecipeRepository recipeRepository) {
-		
-		this.recipeRepository = recipeRepository;
-	}
+    private final RecipeCommandToRecipe recipeCommandToRecipe;
+    private final RecipeToRecipeCommand recipeToRecipeCommand;
 
+    public RecipeServiceImpl(RecipeRepository recipeRepository, RecipeCommandToRecipe recipeCommandToRecipe, RecipeToRecipeCommand recipeToRecipeCommand) {
+        this.recipeRepository = recipeRepository;
+        this.recipeCommandToRecipe = recipeCommandToRecipe;
+        this.recipeToRecipeCommand = recipeToRecipeCommand;
+    }
 
 	@Override
 	public Set<Recipe> getRecipes() {
@@ -44,5 +49,17 @@ public class RecipeServiceImpl implements RecipeService{
 		
 		return recipeOptional.get();
 	}
+
+
+	
+	 @Override
+	    @Transactional
+	    public RecipeCommand saveRecipeCommand(RecipeCommand command) {
+	        Recipe detachedRecipe = recipeCommandToRecipe.convert(command);
+
+	        Recipe savedRecipe = recipeRepository.save(detachedRecipe);
+	        log.debug("Saved RecipeId:" + savedRecipe.getId());
+	        return recipeToRecipeCommand.convert(savedRecipe);
+	    }
 
 }
